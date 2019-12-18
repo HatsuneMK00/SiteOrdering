1e73aa03a6b6d40eeb7c9133a6066e54c5ac048b
mikotokirino <shikirin556@gmail.com>
2019-12-18 15:13:56 +0800

    update: 添加newsmapper

--- src/main/java/xyz/st/meethere/mapper/NewsMapper.java
+++ src/main/java/xyz/st/meethere/mapper/NewsMapper.java
@@ -1,8 +1,28 @@
 package xyz.st.meethere.mapper;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Repository;
+import xyz.st.meethere.entity.News;
+
+import java.util.List;
 
 @Repository
-public class NewsMapper {
+public interface NewsMapper {
+    @Insert("insert into news(title,date,content) values(#{title},#{date},#{content})")
+    News addNews(News news);
 
+    @Update("update news set title=#{title},date=#{date},content=#{content} where newsId=#{newsId}")
+    int updateNews(News news);
+
+    @Delete("delete from news where newsId=#{newsId}")
+    int deleteNews(Integer newsId);
+
+    @Select("select * from news")
+    List<News> getAllNews();
+
+    @Select("select * from news where newsId=#{newsId}")
+    News getNewsByNewsId(Integer newsId);
 }
