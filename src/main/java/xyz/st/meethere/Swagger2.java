148bd54a9ccd2f79baa12a5cab6e1108c0a49bec
ceej <ceej_7@hotmail.com>
2019-12-19 12:14:51 +0800

    fix: swagger issue

--- src/main/java/xyz/st/meethere/Swagger2.java
+++ src/main/java/xyz/st/meethere/Swagger2.java
@@ -6,6 +6,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -28,7 +29,7 @@ public class Swagger2 {
         return new ApiInfoBuilder()
                 .title("MeetHere")
                 .description("软件测试期末项目meethere接口说明文档")
-                .contact("K.makise")
+                .contact(new Contact("K.makise","https://github.com/HatsuneMK00","TODO"))// contact(str) is deprecated
                 .version("1.0")
                 .build();
     }
