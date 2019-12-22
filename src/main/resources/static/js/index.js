8b25966d42be0746aa60a6c2bc352e0ec7729a6c
yuanfeif <1272091180@qq.com>
2019-12-22 19:02:10 +0800

    feat: modify changePwd.html and some related html files but not finished

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -155,10 +155,10 @@ function donation(){
 		area : ['260px', '367px'],
 		tab : [{
 			title : "微信",
-			content : "<div style='padding:30px;overflow:hidden;background:#d2d0d0;'><img src='images/wechat.jpg'></div>"
+			content : "<div style='padding:30px;overflow:hidden;background:#d2d0d0;'><img src='image/wechat.jpg'></div>"
 		},{
 			title : "支付宝",
-			content : "<div style='padding:30px;overflow:hidden;background:#d2d0d0;'><img src='images/alipay.jpg'></div>"
+			content : "<div style='padding:30px;overflow:hidden;background:#d2d0d0;'><img src='image/alipay.jpg'></div>"
 		}]
 	})
 }
