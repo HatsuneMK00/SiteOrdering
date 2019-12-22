f574f44ffef554505cbed7131414c0d76c0edc57
ceej <ceej_7@hotmail.com>
2019-12-23 03:29:46 +0800

    feat: 文件api

---
+++ src/main/java/xyz/st/meethere/controller/FileController.java
@@ -0,0 +1,55 @@
+package xyz.st.meethere.controller;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import xyz.st.meethere.config.MyServerConfig;
+import xyz.st.meethere.entity.ResponseMsg;
+import xyz.st.meethere.entity.User;
+import xyz.st.meethere.exception.FileException;
+import xyz.st.meethere.service.AdminService;
+import xyz.st.meethere.service.FileService;
+
+import java.util.Map;
+
+@RestController
+public class FileController {
+    @Autowired
+    private FileService fileService;
+
+
+    /*
+     * 个人信息头像管理
+     * */
+    @ResponseBody
+    @ApiOperation("上传图片文件,返回url")
+    @PostMapping("/file/uploadImage")
+    ResponseMsg updateProfilePic(@RequestParam("image")MultipartFile file){
+        /*
+         * 封装图片路径
+         * */
+        ResponseMsg responseMsg = new ResponseMsg();
+        responseMsg.setStatus(404);
+        String storeFile = null;
+        try {
+            storeFile = fileService.storeFile(file);
+        } catch (FileException e) {
+            e.printStackTrace();
+            return responseMsg;
+        }
+        if(storeFile == null)
+            return responseMsg;
+        responseMsg.setStatus(200);
+
+        String[] temp = storeFile.split("/");
+        String server = MyServerConfig.server;
+        String port   = MyServerConfig.port;
+        // Default server regarded as [localhost]
+        String profile_url="http://"+server+":"+port+ "/images/";
+        profile_url = profile_url + temp[temp.length - 1];
+        responseMsg.getResponseMap().put("url",profile_url);
+
+        return responseMsg;
+    }
+}
