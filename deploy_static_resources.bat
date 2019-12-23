c359c2ab5f5b80d78bcc22a66bb5d8f10c878a4f
ceej <ceej_7@hotmail.com>
2019-12-23 17:05:39 +0800

    fix: deploying scirpts wo explicit output

--- deploy_static_resources.bat
+++ deploy_static_resources.bat
@@ -1,2 +1,3 @@
-del /S /Q target\classes\static
-XCOPY src\main\resources\static target\classes\static /y /E
+del /S /Q target\classes\static >log.txt
+XCOPY src\main\resources\static target\classes\static /y /E >log.txt
+del /S /Q log.txt
