28cb81d932b7517bbf5bb0fde2409675fba17240
HatsuneMK00 <10175101106@stu.ecnu.edu.cn>
2019-12-23 10:14:30 +0800

    update: 修改了端口号获取的方法 经过了测试可以获取到

--- src/main/java/xyz/st/meethere/config/MyServerConfig.java
+++ src/main/java/xyz/st/meethere/config/MyServerConfig.java
@@ -2,8 +2,8 @@ package xyz.st.meethere.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.web.context.WebServerInitializedEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
@@ -11,7 +11,7 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 @Configuration
-public class MyServerConfig {
+public class MyServerConfig implements ApplicationListener<WebServerInitializedEvent> {
     public static String server;
     public static String port;
 
@@ -22,10 +22,11 @@ public class MyServerConfig {
     * 可以自己控制自动注入和构造函数的执行顺序
     * 让自动注入先于构造函数中的调用
     * */
-    @Autowired
+//    @Autowired
     public MyServerConfig(Environment environment) {
 //        port = environment.getProperty("local.server.port");
-        port="8080";
+//        logger.info("port: " + port);
+//        port="8080";
         try {
             server = InetAddress.getLocalHost().getHostAddress();
             if (server.startsWith("192.168"))
@@ -37,4 +38,9 @@ public class MyServerConfig {
         }
     }
 
+    @Override
+    public void onApplicationEvent(WebServerInitializedEvent webServerInitializedEvent) {
+        port = String.valueOf(webServerInitializedEvent.getWebServer().getPort());
+        logger.info("port: " + port);
+    }
 }
