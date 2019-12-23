8193e0393e821a50bd01367cd25178dba5488400
HatsuneMK00 <10175101106@stu.ecnu.edu.cn>
2019-12-23 13:45:53 +0800

    update: 为Comment添加审核功能

--- src/main/java/xyz/st/meethere/config/MyServerConfig.java
+++ src/main/java/xyz/st/meethere/config/MyServerConfig.java
@@ -36,5 +36,4 @@ public class MyServerConfig {
             server = "localhost";
         }
     }
-
 }
--- src/main/java/xyz/st/meethere/mapper/CommentMapper.java
+++ src/main/java/xyz/st/meethere/mapper/CommentMapper.java
@@ -1,9 +1,7 @@
 package xyz.st.meethere.mapper;
 
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Select;
-import org.apache.ibatis.annotations.Update;
+import io.swagger.models.auth.In;
+import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Repository;
 import xyz.st.meethere.entity.Comment;
 
@@ -23,6 +21,22 @@ public interface CommentMapper {
     @Update("update comment set date=#{date},content=#{content} where groundId=#{groundId} and userId=#{userId}")
     int updateCommentOfUserIdOnGroundId(Integer userId, Integer groundId);
 
-    @Insert("insert into comment(userId,groundId,date,content) values(#{userId},#{groundId},#{date},#{content})")
+    @Options(useGeneratedKeys = true,keyProperty = "commentId")
+    @Insert("insert into comment(userId,groundId,date,content,checked) values(#{userId},#{groundId},#{date},#{content},#{checked})")
     int addCommentOfUserIdOnGroundId(Comment comment);
+
+    @Update("update comment set checked=1 where commentId=#{id}")
+    int updateCommentSetChecked(Integer id);
+
+    @Update("update comment set checked=0 where commentId=#{id}")
+    int updateCommentSetUnchecked(Integer id);
+
+    /*
+    * 管理员操作
+    * */
+    @Select("select * from comment where checked=0")
+    List<Comment> getAllUncheckedComments();
+
+    @Select("select * from comment where checked=1")
+    List<Comment> getAllCheckedComments();
 }
