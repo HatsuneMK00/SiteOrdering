fdd4b98b80cbadf46c83d92a498228f5389d1591
ceej <ceej_7@hotmail.com>
2019-12-26 14:00:06 +0800

    fix js/ -> ../../js/

--- src/main/resources/static/page/comment/admin_comment.js
+++ src/main/resources/static/page/comment/admin_comment.js
@@ -1,5 +1,5 @@
 layui.config({
-    base : "js/"
+    base : "../../js/"
 }).use(['form','layer','jquery','laypage'],function(){
     var form = layui.form(),
         layer = parent.layer === undefined ? layui.layer : parent.layer,
