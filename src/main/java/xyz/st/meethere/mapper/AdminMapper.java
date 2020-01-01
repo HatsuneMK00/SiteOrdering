01092ce71633c65fef0a81b88f60945551a226b0
HatsuneMK00 <10175101106@stu.ecnu.edu.cn>
2020-01-01 17:39:34 +0800

    fix: 修改代码格式

--- src/main/java/xyz/st/meethere/mapper/AdminMapper.java
+++ src/main/java/xyz/st/meethere/mapper/AdminMapper.java
@@ -1,31 +1,58 @@
 package xyz.st.meethere.mapper;
 
-import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Repository;
 import xyz.st.meethere.entity.User;
 
-import java.util.List;
-
 @Repository
 public interface AdminMapper {
+    /**
+     * @param userName
+     * @return User
+     */
     @Select("select * from user where userName=#{userName} and authority=1")
     User getAdminByName(String userName);
 
+    /**
+     * @param userId
+     * @return User
+     */
     @Select("select * from user where userId=#{userId} and authority=1")
     User getAdminById(int userId);
 
+
+    /**
+     * @param userName
+     * @return User
+     */
     @Select("select * from user where userName=#{userName}")
     User getUserByNameWOAuthority(String userName);
 
-    @Update("update user set userName=#{userName}"+
-            ", password=#{password}" +
-            ", email=#{email}" +
-            ", description=#{description}" +
-            ", profilePic=#{profilePic}" +
-            ", balance=#{balance} "+
+    /**
+     * @param user
+     * @return int
+     */
+    @Update("update user set userName=#{userName}"
+            +
+            ", password=#{password}"
+            +
+            ", email=#{email}"
+            +
+            ", description=#{description}"
+            +
+            ", profilePic=#{profilePic}"
+            +
+            ", balance=#{balance} "
+            +
             "where userId=#{userId} and authority=1")
     int updateAdmin(User user);
 
+    /**
+     * @param userId
+     * @param profilePic
+     * @return int
+     */
     @Update("update user set profilePic=#{profilePic} where userId=#{userId}")
     int updateAdminProfilePicByAdminId(Integer userId, String profilePic);
 
